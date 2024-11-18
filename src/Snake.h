85e6388f038cef200da05c12d9e62f438209653d
nullnag <153712889+nullnag@users.noreply.github.com>
2024-11-18 14:38:28 +0500

    Add files via upload

---
+++ src/Snake.h
@@ -0,0 +1,35 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <QObject>
+#include "GameField.h"
+
+enum class Direction {
+    Up,
+    Down,
+    Left,
+    Right
+};
+
+class Snake : public QObject
+{
+    Q_OBJECT
+public:
+    Snake(GameField* gameField, int x, int y);
+
+    void move();
+    void grow();
+    void changeDirection(Direction dir);
+    void setNewHead();
+    Direction getDirection();
+    QPoint calculateNewHead();
+    QPoint getHead();
+    QList<QPoint> getBody();
+private:
+    GameField* gameField;
+    QList<QPoint> body;
+    Direction direction;
+    Direction pendingDirection;
+};
+
+#endif // SNAKE_H
